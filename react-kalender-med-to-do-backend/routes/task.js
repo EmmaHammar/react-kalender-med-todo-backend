d43778a4aa706e401101d41706f6022690efa8b9
Emma Hammar <emmahammar@macbook-air.lan>
2021-09-02 11:57:09 +0200

    sends taskInfo from db to frontend repo

--- react-kalender-med-to-do-backend/routes/task.js
+++ react-kalender-med-to-do-backend/routes/task.js
@@ -1,19 +1,23 @@
 var express = require('express');
 var router = express.Router();
+const cors = require('cors');
+
+router.use(cors());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   req.app.locals.db.collection('taskInfoCollection').find().toArray()
   .then(results => {
-    let printTasks="<div><h2>Våra tasks</h2>"
+    let printTasks="<div><h2>Alla mina tasks</h2><ul>"
     
     for (task in results) {
-      printTasks += "<div>" + results[task].title;
+      printTasks += "<li>" + results[task].date + " | " + results[task].title + " | " + results[task].isFinish + "</li>";
     }
 
-    printTasks += "</div"
+    printTasks += "</ul></div"
 
-    res.send(printTasks)
+    res.send([printTasks])
+    
   });
 });
 
