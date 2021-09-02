90fe70bf0af41b90fad514a3903d2c1c3b40a0f8
Emma Hammar <emmahammar@macbook-air.lan>
2021-09-02 13:36:11 +0200

    add new task to db

--- react-kalender-med-to-do-backend/routes/task.js
+++ react-kalender-med-to-do-backend/routes/task.js
@@ -6,6 +6,8 @@ router.use(cors());
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  //this router prints all tasks (get data from MongoDB)
+
   req.app.locals.db.collection('taskInfoCollection').find().toArray()
   .then(results => {
     let printTasks="<div><h2>Alla mina tasks</h2><ul>"
@@ -22,5 +24,22 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.post('/add', function(req, res) {
+  //this router saves new task into MongoDB
+
+  //hämta?
+  let newTask = 
+  {
+    date: req.body.date,
+    title: req.body.title,
+    isFinish: false
+  };
+
+  req.app.locals.db.collection("taskInfoCollection").insertOne(newTask)
+  .then(result => {
+    res.json( {"code": "newTask saved", "id" : newTask.id} );
+  });
+
+});
 
 module.exports = router;
