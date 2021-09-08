e3a98761897ffe73f6b47a75044a5fa73c489402
Emma Hammar <emmahammar@macbook-air.lan>
2021-09-09 00:28:04 +0200

    change res.send format

--- react-kalender-med-to-do-backend/routes/masterlist.js
+++ react-kalender-med-to-do-backend/routes/masterlist.js
@@ -11,7 +11,7 @@ router.get('/', function(req, res, next) {
     .then(results => {
       console.log("masterResults", results);
   
-      res.send([results])
+      res.send(results)
       
     });
   });
