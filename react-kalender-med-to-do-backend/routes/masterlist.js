dc91e6fe94c78128bf89235b0195c74c1979ced4
Emma Hammar <emmahammar@macbook-air.lan>
2021-09-10 09:26:09 +0200

    moving sort() to backend

--- react-kalender-med-to-do-backend/routes/masterlist.js
+++ react-kalender-med-to-do-backend/routes/masterlist.js
@@ -7,10 +7,13 @@ router.use(cors());
 router.get('/', function(req, res, next) {
     //this router gets all task data from MongoDB)
   
-    req.app.locals.db.collection('taskInfoCollection').find().toArray()
+    // req.app.locals.db.collection('taskInfoCollection').find().toArray()
+    req.app.locals.db.collection('taskInfoCollection').find().sort({date:1}).toArray()
+
     .then(results => {
       console.log("masterResults", results);
-  
+
+      console.log("results är sorterat??", results); 
       res.send(results)
       
     });
