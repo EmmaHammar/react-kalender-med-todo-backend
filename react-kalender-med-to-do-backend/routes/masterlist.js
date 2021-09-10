fa9a8aa2097e0e09ca23f545bc9b2e4e31028e19
Emma Hammar <emmahammar@macbook-air.lan>
2021-09-10 10:28:06 +0200

    changes back sort() to frontend

--- react-kalender-med-to-do-backend/routes/masterlist.js
+++ react-kalender-med-to-do-backend/routes/masterlist.js
@@ -7,8 +7,8 @@ router.use(cors());
 router.get('/', function(req, res, next) {
     //this router gets all task data from MongoDB)
   
-    // req.app.locals.db.collection('taskInfoCollection').find().toArray()
-    req.app.locals.db.collection('taskInfoCollection').find().sort({date:1}).toArray()
+    req.app.locals.db.collection('taskInfoCollection').find().toArray()
+    // req.app.locals.db.collection('taskInfoCollection').find().sort({date:1}).toArray()
 
     .then(results => {
       console.log("masterResults", results);
