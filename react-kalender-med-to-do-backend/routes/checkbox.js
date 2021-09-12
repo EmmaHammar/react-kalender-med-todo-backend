bc66cb2e14f0fdaa752f2cffd9b5326ac9bf4dcd
Emma Hammar <emmahammar@macbook-air.lan>
2021-09-12 12:00:43 +0200

    solving issues from refresh

--- react-kalender-med-to-do-backend/routes/checkbox.js
+++ react-kalender-med-to-do-backend/routes/checkbox.js
@@ -8,12 +8,25 @@ router.use(cors());
 
 router.post('/', function(req, res) {
   //this router updates checkbox boolean when checked
-  console.log("req.body.id", req.body.id);
-  console.log("req.body.id", req.body.id);
+  console.log("req.body.id - ja detta klickade id-et", req.body.id);
+  // console.log("req.body.title", req.body.title);
+  // console.log("req.body.date", req.body.date);
+  // console.log("req.body.isFinish", req.body.isFinish);
 
-  console.log("req.body.isFinish", req.body.isFinish);
+  let deleteTask = 
+  {
+    date: req.body.date,
+    title: req.body.title,
+    isFinish: false,
+    id: req.body.id 
+  };
+
+  console.log("HÄR deleteTask, id ibland ''", deleteTask);
+
+
+
   // console.log("matcha obj id", 'ObjectId("'+(req.body.id)+'")' );
-  console.log("objid", mongodb.ObjectId(req.body.id));
+  // console.log("objid", mongodb.ObjectId(req.body.id));
 
   //uppdaterar isFinish:
   // req.app.locals.db.collection("taskInfoCollection").updateOne( {"_id" : mongodb.ObjectId(req.body.id) }, {$set: {"isFinish" : req.body.isFinish} })
@@ -22,10 +35,12 @@ router.post('/', function(req, res) {
   //   res.json( {"code": "updateTask", "updateTaskId": req.body.id} );
   // });
 
-  req.app.locals.db.collection("taskInfoCollection").remove( {"_id" : mongodb.ObjectId(req.body.id)} )
+
+  //denna funkar 
+  req.app.locals.db.collection("taskInfoCollection").deleteOne( {id : req.body.id} )
   .then(result => {
-    // console.log("result", result);
-    res.json( {"code": "updateTask", "updateTaskId": req.body.id} );
+    console.log("result", result);
+    res.json( {"code": "delete Task", "deletedTaskId": req.body.id} );
   });
 
 });
--- react-kalender-med-to-do-backend/routes/task.js
+++ react-kalender-med-to-do-backend/routes/task.js
@@ -7,15 +7,18 @@ router.use(cors());
 router.post('/', function(req, res) {
   //this router saves new task into MongoDB
 
+  console.log("req.body.id:", req.body.id);
 
   //hämta?
   let newTask = 
   {
     date: req.body.date,
     title: req.body.title,
-    isFinish: false
+    isFinish: false,
+    id: req.body.id //här får den ObjectId men borde ha bara id
   };
-  console.log("newTask:", newTask);
+  // console.log("newTask:", newTask);
+  
 
 
   req.app.locals.db.collection("taskInfoCollection").insertOne(newTask)
